d9decfb9c4be2b64b0c5f4314d1ea9e840c5be60
wwjoon <manna.wwjoon@gmail.com>
2022-07-21 18:11:39 +0900

    로컬 앱 설정, 인그레스 get,post 구분 불가 문제로 create path로 변경

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://my-app.org/posts", {
+    await axios.post("http://my-app.org/posts/create", {
       title,
     });
 
--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex'); // hex, 16진수
   const { title }  = req.body;
 
